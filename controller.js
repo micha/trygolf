5efb167e659d33f9f840e55b5954bb17c2ee455d
Micha Niskin <asdf@ubergibson.com>
2010-02-28 06:08:53 -0500

    asdf

--- controller.js
+++ controller.js
@@ -3,16 +3,26 @@ $(document).unbind("route_error").bind("route_error", function(e) {
   alert("Exception: "+e.message);
 });
 
+$.golf.location = function(hash) {
+  $.golf.route(hash);
+};
+
+$("a").live("click", function(event) {
+  $.golf.location(this.href.replace(/^#/, ""));
+});
+
 $.golf.controller = [
 
   { route: "/test/",
     action: function(container, params) {
+      document.title = "Try Golf > Test";
       container.empty().append(new Component.Test());
     }
   },
 
   { route: "/home/",
     action: function(container, params) {
+      document.title = "Try Golf";
       container.empty().append(new Component.Main());
     }
   }
